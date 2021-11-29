589fde64cba8ee2ab66bea3b6ebe77ed29664fb8
kailafitz <mikhailafitzpatrick@yahoo.com>
2021-11-29 20:37:46 +0000

    Python folder structure

--- api/app.py
+++ api/app.py
@@ -1,25 +1,6 @@
-# https://pypi.org/project/virtualenv/
-# https://pypi.org/project/python-dotenv/
+from Project import create_app
 
-from flask import Flask
-from flask_cors import CORS, cross_origin
-
-app = Flask(__name__)
-CORS(app)
-
-
-@app.route("/")
-def index():
-    return "<h1>Welcome to the backend!</h1>"
-
-
-@app.route("/api", methods=["GET"])
-@cross_origin()
-def test():
-    return {
-        "name": "Kaila"
-    }
-
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
